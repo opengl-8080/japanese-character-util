91f65d591a4cc53092cdfdd0b8310b91391cbafc
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-10 01:36:37 +0900

    アスキー文字判定

--- src/main/java/com/github/opengl8080/kanatil/AsciiCharacter.java
+++ src/main/java/com/github/opengl8080/kanatil/AsciiCharacter.java
@@ -2,4 +2,8 @@ package com.github.opengl8080.kanatil;
 
 public class AsciiCharacter {
 
+    public static boolean isAscii(char c) {
+        return 0x0020 <= c && c <= 0x007E;
+    }
+
 }
--- src/test/java/com/github/opengl8080/kanatil/AsciiCharacterTest.java
+++ src/test/java/com/github/opengl8080/kanatil/AsciiCharacterTest.java
@@ -1,6 +1,6 @@
 package com.github.opengl8080.kanatil;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,8 +12,20 @@ public class AsciiCharacterTest {
     
     
     @Test
-    public void test() {
-        fail("まだ実装されていません");
+    public void アスキー文字はtrue() {
+        for (char c : Characters.ASCII) {
+            assertThat(AsciiCharacter.isAscii(c))
+                .as(c + "")
+                .isTrue();
+        }
     }
 
+    @Test
+    public void 半角ｶﾅはfalse() {
+        for (char c : Characters.HALFWIDTH) {
+            assertThat(AsciiCharacter.isAscii(c))
+                .as(c + "")
+                .isFalse();
+        }
+    }
 }
